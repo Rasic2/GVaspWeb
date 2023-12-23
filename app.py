909427c1890e897f186282362ebc3efcdf9d43d2
Rasic2 <1051987201@qq.com>
2023-12-23 17:37:39 +0800

    add interface

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    # 生成绘制图表所需要的数据
+@app.route('/', methods=['POST', 'GET'])
+def plot():
     x = [1, 2, 3, 4]
     y = [3, 5, 2, 6]
 
-    # 使用Matplotlib绘制数据图表
-    plt.plot(x, y)
+    line_width = 1
+    if request.method == "POST":
+        line_width = request.form['lineWidth']
 
+    plt.plot(x, y, color="r", linewidth=line_width)
     plt.savefig("static/image.svg")
+    plt.close()
 
     return render_template('plot.html', svg_name="static/image.svg")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
