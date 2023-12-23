e1d54561b63bea36e6835e6fee4cef27c62ed0a3
Rasic2 <1051987201@qq.com>
2023-12-23 19:02:29 +0800

    add style.css

--- static/plot.js
+++ static/plot.js
@@ -1,7 +1,27 @@
+function checkNum() {
+  const inputNum = document.querySelector("#inputData");
+  // console.log(inputNum);
+  boolNum = !isNaN(parseFloat(inputNum.value)) && isFinite(inputNum.value);
+  if (!boolNum) {
+    document.querySelector("input[type=submit]").disabled = true;
+    inputNum.parentElement.appendChild(document.createElement("p"));
+    document.querySelector("p").innerHTML = "格式错误";
+    document.querySelector("p").className = "formatError";
+  } else {
+    document.querySelector("input[type=submit]").disabled = false;
+    if (document.querySelector("p") != null) {
+      document
+        .querySelector("p")
+        .parentElement.removeChild(document.querySelector("p"));
+      inputNum.className = "";
+    }
+  }
+}
+
 function plot() {
-  const button = document.querySelector("input[type=button]");
+  const submit = document.querySelector("input[type=submit]");
   const embed = document.querySelector("embed");
-  button.onclick = function () {
+  submit.onclick = function () {
     embed.src = "../static/image.svg";
   };
 }
