281840e95004c737e58156073ca6103266fd3809
Rasic2 <1051987201@qq.com>
2023-12-24 16:35:58 +0800

    add svg in vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>GVasp 态密度图绘制</h1>
+  <img :src="svg" alt />
+  <!-- <embed src="../../static/image.svg" /> -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
+  data() { return { svg: require("../../static/image.svg") } }
 }
 </script>
 
