aa2db1595444c9019b96a95a517430e8f173f5b0
Rasic2 <1051987201@qq.com>
2024-07-05 21:51:46 +0800

    fix: ValueError: Shape of passed values is (2000, 18), indices imply (2000, 13)

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import os
-import sqlite3
-import uuid
+from hashlib import md5
+from pathlib import Path
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
 from flask_cors import cross_origin, CORS
 from gvasp.common.file import OUTCAR, LOCPOT, CONTCAR
 from gvasp.common.plot import DOSData
@@ -12,19 +12,10 @@ CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 设置最大文件上传大小为 100MB
 
 # 创建保存文件的目录
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-# 初始化数据库
-conn = sqlite3.connect('file_mapping.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS files
-             (id INTEGER PRIMARY KEY, original_filename TEXT, new_filename TEXT)''')
-conn.commit()
-conn.close()
+UploadPath = Path(UPLOAD_FOLDER)
+UploadPath.mkdir(exist_ok=True)
 
 
 # @app.route("/")
@@ -44,39 +35,41 @@ def upload_file():
         if 'file' not in request.files:
             return 'No file part'
         file = request.files['file']
-        conn = sqlite3.connect('file_mapping.db')
-        c = conn.cursor()
+        chunk_index = request.form['chunkIndex']
+        total_chunks = request.form['totalChunks']
 
         if request.form['filename'] == '':
             return '没有选择文件'
         try:
             original_filename = request.form['filename']
-            # 查询数据库，检查文件名是否已经存在
-            c.execute("SELECT * FROM files WHERE original_filename=?", (original_filename,))
-            existing_file = c.fetchone()
-            if existing_file:
-                new_filename = existing_file[2]
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
-            else:
-                # 生成唯一的文件名
-                new_filename = str(uuid.uuid4()) + os.path.splitext(original_filename)[1]
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
-                file.save(file_path)
-                # 存储原始文件名和新文件名的关联关系到数据库
-                c.execute("INSERT INTO files (original_filename, new_filename) VALUES (?, ?)",
-                          (original_filename, new_filename))
-            conn.commit()
-            try:
-                contcar = CONTCAR(file_path)
-                _ = contcar.structure
-                file_content = ''.join(contcar.strings)
-            except Exception:
-                file_content = None
-            return {"filePath": file_path, "fileContent": file_content}
+            md5_object = md5()
+            md5_object.update(original_filename.encode('utf-8'))
+            new_filename = md5_object.hexdigest() + os.path.splitext(original_filename)[1]
+            file_path = UploadPath / new_filename
+            file_part_path = str(file_path) + f".{chunk_index}"
+            file.save(file_part_path)
+
+            with open(file_part_path, "r") as f:
+                file_line_count = len(f.readlines())
+            file_content = None
+
+            if int(chunk_index) == int(total_chunks) - 1:
+                with open(file_path, 'wb') as output_file:
+                    for i in range(int(total_chunks)):
+                        with open(UploadPath / f"{new_filename}.{i}", 'rb') as part_file:
+                            output_file.write(part_file.read())
+                        os.remove(UploadPath / f"{new_filename}.{i}")
+
+                try:
+                    contcar = CONTCAR(file_path)
+                    _ = contcar.structure
+                    file_content = ''.join(contcar.strings)
+                except Exception:
+                    file_content = None
+            return {"filePath": str(file_path.relative_to("./")), "fileContent": file_content,
+                    "fileLineCount": file_line_count}
         except Exception as e:
             return '文件上载过程中出错: {}'.format(str(e))
-        finally:
-            conn.close()
     else:
         return '请求方法不允许'
 
