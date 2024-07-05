041d0cc94425b051aa9185ddf8384b9f957fbbf1
Rasic2 <1051987201@qq.com>
2024-07-05 15:21:14 +0800

    ci: update deploy.sh for uncomment the app.py

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+comment_start=$(cat -n backend/app.py | grep serve_index -B1 | head -1 | awk '{print $1}')
+comment_end=$(cat -n backend/app.py | grep serve_static -A1 | tail -1 | awk '{print $1}')
+echo "Uncomment the app.py"
+sed -i "${comment_start},${comment_end} s/^# //" "backend/app.py"
+
 cd frontend || exit
 echo "Build Fronted Project"
 npm run build
