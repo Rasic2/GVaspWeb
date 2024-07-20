f8e6556c4f73f28eff8f1a56b80099cd999856df
Rasic2 <1051987201@qq.com>
2024-07-20 16:34:42 +0800

    feat: add TrajDisplay

--- frontend/src/components/TrajDisplay.vue
+++ frontend/src/components/TrajDisplay.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="trajViewer" class="trajViewer"></div>
-  <button @click="playPause">{{ isPlaying ? '暂停' : '播放' }}</button>
-  <button @click="nextFrame">下一帧</button>
+  <div class="trajDisplay">
+    <div class="trajControl">
+      <div class="trajButton">
+        <el-button @click="playPause">{{ isPlaying ? '暂停' : '播放' }}</el-button>
+        <el-button @click="nextFrame">下一帧</el-button>
+      </div>
+      <p>{{ currentFrame }} / {{ props.trajData.length }}</p>
+    </div>
+    <div id="trajViewer" class="trajViewer"></div>
+  </div>
 </template>
 
 <script>
@@ -11,7 +18,7 @@ export default {
 </script>
 
 <script setup>
-import { onMounted, defineProps } from "vue";
+import { ref, onMounted, defineProps } from "vue";
 import $ from "jquery";
 
 const props = defineProps({
@@ -21,30 +28,100 @@ const props = defineProps({
   },
 })
 
-const display = () => {
+const viewer = ref(null)
+const isPlaying = ref(false);
+const currentFrame = ref(0);
+const currentView = ref(null)
+let playInterval = null;
+
+const saveView = () => {
+  if (viewer.value) {
+    // 保存视角信息
+    currentView.value = {
+      camera: viewer.value.camera.position.clone(),
+      rotation: viewer.value.rotationGroup.rotation.clone(),
+      center: viewer.value.rotationGroup.position.clone(),
+    };
+  }
+};
+
+const restoreView = () => {
+  if (viewer.value && currentView.value) {
+    // 恢复视角信息
+    viewer.value.camera.position.copy(currentView.value.camera);
+    viewer.value.rotationGroup.rotation.copy(currentView.value.rotation);
+    viewer.value.rotationGroup.position.copy(currentView.value.center);
+    viewer.value.render();
+  }
+};
+
+const loadFrame = (frameIndex) => {
   let config = { backgroundColor: "white" };
   const defaultStyle = {
     stick: { radius: 0.2, colorscheme: "Jmol" },
     sphere: { scale: 0.35, colorscheme: "Jmol" },
   };
   import("../../../../3Dmol.js/build/3Dmol-min").then(($3Dmol) => {
-    const viewer = $3Dmol.createViewer($("#trajViewer"), config);
-    console.log(viewer)
-    let m = viewer.addModel(props.trajData[0], "vasp");
-    viewer.setStyle({}, defaultStyle);
-    viewer.render()
+    if (!viewer.value) {
+      viewer.value = $3Dmol.createViewer($("#trajViewer"), config);
+    }
+    saveView()
+    viewer.value.removeAllModels();
+    let m = viewer.value.addModel(props.trajData[frameIndex], "vasp");
+    viewer.value.addUnitCell(m, { alabel: 'A', blabel: 'B', clabel: 'C', astyle: { color: 'red', radius: 0.2, midpos: -1 }, bstyle: { color: 'green', radius: 0.2, midpos: -1 }, cstyle: { color: 'blue', radius: 0.2, midpos: -1 } });
+    viewer.value.setStyle({}, defaultStyle);
+    viewer.value.zoomTo();
+    viewer.value.render()
+
+    restoreView()
   })
 };
-onMounted(display);
+
+const playPause = () => {
+  if (isPlaying.value) {
+    clearInterval(playInterval);
+  } else {
+    playInterval = setInterval(() => {
+      currentFrame.value = (currentFrame.value + 1) % props.trajData.length;
+      loadFrame(currentFrame.value);
+    }, 1000); // 每秒播放一帧
+  }
+  isPlaying.value = !isPlaying.value;
+};
+
+const nextFrame = () => {
+  if (!isPlaying.value) {
+    currentFrame.value = (currentFrame.value + 1) % props.trajData.length;
+    loadFrame(currentFrame.value);
+  }
+};
+
+onMounted(() => {
+  loadFrame(currentFrame.value);
+});
 </script>
 
 <style>
 .trajViewer {
+  display: flex;
+}
+
+.trajButton {
+  margin-right: 10px;
+}
+
+.trajControl {
+  float: top;
+  display: flex;
+  align-items: center;
+}
+
+.trajViewer {
   margin-top: 10px;
   margin-bottom: 10px;
-  margin-left: 25px;
   height: 430px;
   width: 500px;
   position: relative;
+  float: left
 }
 </style>
--- frontend/src/components/VisTraj.vue
+++ frontend/src/components/VisTraj.vue
@@ -3,9 +3,8 @@
     <div class="upload_plot">
       <file-upload class="upload" :index="index" :limitNum="1" :type="'XDATCAR'" :uploadTip="'请上传 XDATCAR 文件'"
         :fileList="uploadItem.fileLists" @uploadSuccess="handleFileLists" @updateFile="updateFileLists"></file-upload>
-      <el-button class="expand_btn" @click="render" :disabled="renderDisabled">渲染</el-button>
     </div>
-    <traj-display :trajData="trajData"></traj-display>
+    <traj-display v-if="trajData.length > 0" :trajData="trajData"></traj-display>
   </div>
 </template>
 
@@ -16,37 +15,13 @@ export default {
 </script>
 
 <script setup>
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import fileUpload from "@/components/upload.vue";
 import TrajDisplay from "@/components/TrajDisplay.vue";
 
 // Ref Variable
 const uploadItem = ref({ fileLists: [] })
 const trajData = ref([])
-
-// Computed
-const fileCount = computed(() => {
-  return uploadItem.value.fileLists.length
-})
-
-/**
- * Whether to disable the plot button.
- *
- * 1. The number of fileLists is not equal to 2
- * 2. Each item in the items array should have a non-empty input and a non-empty orbitals array if the radio is not 1
- * 3. The items array should not be empty if checkedTDOS is false
- */
-const renderDisabled = computed(() => {
-  // Check if the fileLists length is not equal to 2
-  if (fileCount.value !== 1) {
-    return true;
-  }
-  return false;
-});
-
-const render = () => {
-  console.log(uploadItem.value.fileLists)
-}
 
 /**
  * Handles the file list for a specific index.
@@ -57,8 +32,7 @@ const render = () => {
 const handleFileLists = (index, file) => {
   console.log("handleFileLists: file", file);
   if (file['fileContent']) {
-    console.log("file content", file['fileContent'])
-    // structureFileContent.value = file['fileContent'];
+    trajData.value = file['fileContent']
   }
   uploadItem.value.fileLists.push(file);
   console.log("handleFileLists: fileLists", uploadItem.value);
