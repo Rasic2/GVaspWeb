d99822cef4ffcc002961599589b5c7c912dc6e7b
Rasic2 <1051987201@qq.com>
2024-08-08 16:01:19 +0800

    feat: add various params

--- frontend/src/components/CreateInput.vue
+++ frontend/src/components/CreateInput.vue
@@ -1,23 +1,43 @@
 <template>
-    <el-form-item v-for="(item, index) in ['任务类型']" :key="index" :label="item" prop="name" class="horizontal-align"
-        required>
-        <el-radio-group v-model="radio" class="inputRadio">
-            <el-radio value="1" size="large">opt</el-radio>
-            <el-radio value="2" size="large">chg</el-radio>
-            <el-radio value="3" size="large">wf</el-radio>
-            <el-radio value="4" size="large">dos</el-radio>
-            <el-radio value="5" size="large">freq</el-radio>
-            <el-radio value="6" size="large">md</el-radio>
-            <el-radio value="7" size="large">stm</el-radio>
-            <el-radio value="8" size="large">ts</el-radio>
-        </el-radio-group>
-    </el-form-item>
-    <div v-if="radio == 1" class="inputOptions">
-        <checkbox-pdos :sIndex="index" :l-orbital="'全选'"
-            :orbitals="['potential', 'vdw', 'sol', 'gamma', 'hse', 'static', 'nelect']"
-            customSelectAll="customSelectAll" customSelectSingle="customSelectSingle"
-            @updateOrbitals="handleUpdateOrbitals"></checkbox-pdos>
+    <div style="display:grid;">
+        <div style="display:flex; align-items:top; grid-template-columns: 1fr; gap:45px">
+            <el-form-item :label="'文件'" required></el-form-item>
+            <file-upload :index="index" :limitNum="1" :type="'OUTCAR'" :uploadTip="'请上传 *.xsd 文件'"
+                :fileList="uploadItem.fileLists" @uploadSuccess="handleFileLists"
+                @updateFile="updateFileLists"></file-upload>
+        </div>
+        <el-form-item v-for="(item, index) in ['任务类型']" :key="index" :label="item" class="horizontal-align" required>
+            <el-radio-group v-model="radio" class="inputRadio">
+                <el-radio value="1" size="large">opt</el-radio>
+                <el-radio value="2" size="large">chg</el-radio>
+                <el-radio value="3" size="large">wf</el-radio>
+                <el-radio value="4" size="large">dos</el-radio>
+                <el-radio value="5" size="large">freq</el-radio>
+                <el-radio value="6" size="large">md</el-radio>
+                <el-radio value="7" size="large">stm</el-radio>
+                <el-radio value="8" size="large">ts</el-radio>
+            </el-radio-group>
+        </el-form-item>
+        <div v-if="radio == 1" class="inputOptions">
+            <checkbox-pdos :sIndex="index" :l-orbital="'全选'"
+                :orbitals="['potential', 'vdw', 'sol', 'gamma', 'hse', 'static', 'nelect']"
+                customSelectAll="customSelectAll" customSelectSingle="customSelectSingle"
+                @updateOrbitals="handleUpdateOrbitals"></checkbox-pdos>
+            <div v-if="options.indexOf('potential') !== -1">
+                <el-form-item label="potential" required>
+                    <el-select v-model="potentialValue" clearable placeholder="Select potential" style="width: 240px">
+                        <el-option v-for="item in potentialOptions" :key="item" :label="item" :value="item" />
+                    </el-select>
+                </el-form-item>
+            </div>
+            <div v-if="options.indexOf('nelect') !== -1">
+                <el-form-item label="NELECT" required>
+                    <el-input-number v-model="nelect" :precision="2" :step="0.1" :max="10" />
+                </el-form-item>
+            </div>
+        </div>
     </div>
+    <el-button class="expand_btn" @click="generate" :disabled="generateDisabled">生成</el-button>
 </template>
 
 <script>
@@ -27,11 +47,65 @@ export default {
 </script>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import CheckboxPdos from "@/components/CheckboxPdos.vue";
+import FileUpload from "@/components/upload.vue";
+
+// Ref Variable
+const uploadItem = ref({ fileLists: [] })
 
 const radio = ref(null)
 const options = ref([])
+
+const potentialValue = ref('PAW_PBE')
+const potentialOptions = ['PAW_LDA', 'PAW_PBE', 'PAW_PW91', 'USPP_LDA', 'USPP_PW91']
+const nelect = ref(0)
+
+// Computed
+
+const fileCount = computed(() => {
+    return uploadItem.value.fileLists.length
+})
+
+const generateDisabled = computed(() => {
+    // Check if the fileLists length is not equal to 2
+    if (fileCount.value !== 1) {
+        return true;
+    }
+
+    if (options.value.indexOf('potential') !== -1) {
+        return potentialValue.value == "" || potentialValue.value == undefined
+    }
+
+    return false;
+})
+
+/**
+ * Handles the file list for a specific index.
+ *
+ * @param {number} index - The index of the item in the items array.
+ * @param {Object} file - The file to be added to the file list.
+ */
+const handleFileLists = (index, file) => {
+    console.log("handleFileLists: file", file);
+    if (file['fileContent']) {
+        console.log("file content", file['fileContent'])
+    }
+    uploadItem.value.fileLists.push(file);
+    console.log("handleFileLists: fileLists", uploadItem.value);
+};
+
+/**
+ * Updates the file lists for a specific index.
+ *
+ * @param {number} index - The index of the item in the uploadItems array.
+ * @param {Array} files - The new file list to be assigned to the item.
+ * @return {void}
+ */
+const updateFileLists = (index, files) => {
+    console.log("updateFileLists: files", files);
+    uploadItem.value.fileLists = files;
+};
 
 const handleUpdateOrbitals = (mode, index, orbitals) => {
     if (!Array.isArray(orbitals)) {
@@ -54,7 +128,6 @@ const handleUpdateOrbitals = (mode, index, orbitals) => {
     }
     console.log(options.value)
 };
-
 </script>
 
 <style>
@@ -67,6 +140,11 @@ const handleUpdateOrbitals = (mode, index, orbitals) => {
     margin-left: 15px;
 }
 
+.inputOptions {
+    display: grid;
+    gap: 18px;
+}
+
 .customSelectAll {
     padding: 0 10px;
     width: 50px;
@@ -77,5 +155,11 @@ const handleUpdateOrbitals = (mode, index, orbitals) => {
     padding: 0 30px;
     width: 20px;
     margin-right: 20px;
+}
+
+.expand_btn {
+    float: left;
+    margin-top: 5px;
+    margin-left: 0px;
 }
 </style>
